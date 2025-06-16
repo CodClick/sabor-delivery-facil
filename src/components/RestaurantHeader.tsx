2ee9910d746cae77da8f4235be8c1e76259e10d4
CodClick <codclick.marketing@gmail.com>
2025-06-16 01:51:48 -0300

    Update RestaurantHeader.tsx

--- src/components/RestaurantHeader.tsx
+++ src/components/RestaurantHeader.tsx
@@ -20,7 +20,7 @@ const RestaurantHeader: React.FC = () => {
           <div className="flex flex-col sm:flex-row items-center">
             <div className="w-24 h-24 rounded-full overflow-hidden border-4 border-white shadow-md mr-0 sm:mr-6 mb-4 sm:mb-0">
               <img
-                src="/images/logo_delivery_facil.png"
+                src="/images/logo_delivery_2.png"
                 alt="Logo"
                 className="w-full h-full object-cover"
                 onError={(e) => {
