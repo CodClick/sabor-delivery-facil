a7fe2b80708fbaafb2ffcbec64f75963d4336f6d
CodClick <codclick.marketing@gmail.com>
2025-06-19 20:05:18 -0300

    Update AdminOrders.tsx

--- src/pages/AdminOrders.tsx
+++ src/pages/AdminOrders.tsx
@@ -252,8 +252,8 @@ const AdminOrders = () => {
     <div className="container mx-auto px-4 py-8">
       <div className="flex justify-between items-center mb-6">
         <h1 className="text-2xl font-bold">Gerenciamento de Pedidos</h1>
-        <Button onClick={() => navigate("/admin")} variant="outline">
-          Voltar para o Cardápio
+        <Button onClick={() => navigate("/admin-dashboard")} variant="outline">
+          Pagina de Administração 
         </Button>
       </div>
 
