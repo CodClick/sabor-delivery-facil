07a1c789c3780e228343d3f1f2ac2adf7b8934fa
CodClick <codclick.marketing@gmail.com>
2025-09-23 17:24:48 -0300

    Update useUserRole.ts

--- src/hooks/useUserRole.ts
+++ src/hooks/useUserRole.ts
@@ -17,7 +17,7 @@ export const useUserRole = () => {
 
       try {
         const { data, error } = await supabase
-          .from("usuarios")
+          .from("users")
           .select("role")
           .eq("firebase_id", currentUser.uid)
           .single();
