372ff8b84d5fbf17249735bd90502a0b02def9c1
CodClick <codclick.marketing@gmail.com>
2025-09-30 14:49:47 -0300

    Create trackingConfig.js

---
+++ src/config/trackingConfig.js
@@ -0,0 +1,10 @@
+// src/config/trackingConfig.js
+
+// ATENÇÃO: Estes são IDs de teste. Troque pelos seus para testar.
+// Pegue um Pixel de teste da sua conta do Facebook Business
+// e um ID de uma propriedade de teste do Google Analytics 4.
+
+export const TRACKING_IDS = {
+  META_PIXEL_ID: 'SEU_ID_DE_PIXEL_AQUI', // Ex: '1234567890123456'
+  GOOGLE_TAG_ID: 'SEU_ID_DE_TAG_DO_GOOGLE_AQUI' // Ex: 'G-ABCDEFGHIJ'
+};
