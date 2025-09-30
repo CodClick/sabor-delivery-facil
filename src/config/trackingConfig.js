0da6891ab0295420ffe186fd1c09cfea4d246145
CodClick <codclick.marketing@gmail.com>
2025-09-30 15:03:35 -0300

    Update trackingConfig.js

--- src/config/trackingConfig.js
+++ src/config/trackingConfig.js
@@ -5,6 +5,6 @@
 // e um ID de uma propriedade de teste do Google Analytics 4.
 
 export const TRACKING_IDS = {
-  META_PIXEL_ID: 'SEU_ID_DE_PIXEL_AQUI', // Ex: '1234567890123456'
-  GOOGLE_TAG_ID: 'SEU_ID_DE_TAG_DO_GOOGLE_AQUI' // Ex: 'G-ABCDEFGHIJ'
+  META_PIXEL_ID: '1056176321723068',
+  GOOGLE_TAG_ID: 'G-1VCTGX0S17'
 };
