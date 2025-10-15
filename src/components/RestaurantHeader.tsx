1fa18bab09a562d4bddd2e9febbbcd3087301c35
CodClick <codclick.marketing@gmail.com>
2025-10-15 16:04:26 -0300

    Add informational text about image usage

--- src/components/RestaurantHeader.tsx
+++ src/components/RestaurantHeader.tsx
@@ -33,6 +33,8 @@ const RestaurantHeader: React.FC = () => {
               <h1 className="text-2xl sm:text-3xl font-bold text-gray-800">Oliveira Pizzaria</h1>
               <p className="text-gray-600 mt-1"><b>Rua Nina Zanoto, 646</b></p>
               <p className="text-gray-600 mt-1"><b>Jd. Las Vegas - Santo André</b></p>
+                                    <p>&nbsp;</p>
+              <p className="text-gray-600 mt-1">As imagens utilizadas são ilustrativas produzidas com inteligência Artificial</p> 
               <div className="flex items-center justify-center sm:justify-start mt-2">
                 <div className="flex text-yellow-400">
                   {[1, 2, 3, 4, 5].map((star) => (
@@ -46,8 +48,6 @@ const RestaurantHeader: React.FC = () => {
             </div>               
           </div>
         </div>
-                      <p>&nbsp;</p>
-              <p className="text-gray-600 mt-1">As imagens utilizadas são ilustrativas produzidas com inteligência Artificial</p> 
       </div>
     </div>
   );
