70d0999eafae37f3b6acd38deffa08b4968d0819
CodClick <codclick.marketing@gmail.com>
2025-10-15 15:57:49 -0300

    Fix formatting and update rating display in RestaurantHeader

--- src/components/RestaurantHeader.tsx
+++ src/components/RestaurantHeader.tsx
@@ -39,10 +39,10 @@ const RestaurantHeader: React.FC = () => {
                     <svg key={star} className="w-5 h-5 fill-current" viewBox="0 0 24 24">
                       <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" />
                     </svg>
-                  ))}
-              <p>&nbsp;</p>
-              <p className="text-gray-600 mt-1">As imagens utilizadas são ilustrativas produzidas com inteligência Artificial</p>                  
+                  ))}             
                 </div>
+              <p>&nbsp;</p>
+              <p className="text-gray-600 mt-1">As imagens utilizadas são ilustrativas produzidas com inteligência Artificial</p>                     
                 <span className="text-gray-600 ml-2">4.8 (120+)</span>
               </div>
             </div>
