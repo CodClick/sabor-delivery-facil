60e2a29650ebbe03c6d370453cc7ffcf8e096ad3
CodClick <codclick.marketing@gmail.com>
2026-01-05 23:46:34 -0300

    Update ChatAssistant.tsx

--- src/components/ChatAssistant.tsx
+++ src/components/ChatAssistant.tsx
@@ -83,7 +83,7 @@ const ChatAssistant = () => {
       setMessages([
         {
           from: "assistant",
-          text: "text: "Olá 👋! Sou o atendente virtual da Pizzaria Oliveira ! Posso te ajudar com o cardápio, preços, promoções ou acompanhar seu pedido 😊",
+          text: "text: "Olá 👋! Sou o atendente virtual da Pizzaria Oliveira ! Posso te ajudar com informações ou acompanhar seu pedido 😊",
         },
       ]);
     }
@@ -175,6 +175,7 @@ const ChatAssistant = () => {
 };
 
 export default ChatAssistant;
+
 
 
 
